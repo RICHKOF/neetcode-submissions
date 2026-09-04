5273edb934b76b29f199c8287a6428d669caab70
RICHKOF <rich.kofi0006@gmail.com>
2026-09-04 19:45:53 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group={}
+        for words in strs:
+            key=tuple(sorted(words))
+            if key in group:
+                group[key].append(words)
+            else:
+                group[key]=[words]
+        return list(group.values())
